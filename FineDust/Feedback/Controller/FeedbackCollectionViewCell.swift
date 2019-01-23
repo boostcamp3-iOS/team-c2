70dc03aae8a3c69948b3c185019d33ad3a371542
Jae-eun <je4297@naver.com>
2019-01-23 15:05:41 +0900

    3번째 탭 화면 컬렉션 뷰 만듦

---
+++ FineDust/Feedback/Controller/FeedbackCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedbackCollectionViewCell.swift
+//  FineDust
+//
+//  Created by 이재은 on 23/01/2019.
+//  Copyright © 2019 boostcamp3rd. All rights reserved.
+//
+
+import UIKit
+
+class FeedbackCollectionViewCell: UICollectionViewCell {
+
+  @IBOutlet weak var feedbackImageView: UIImageView!
+}
