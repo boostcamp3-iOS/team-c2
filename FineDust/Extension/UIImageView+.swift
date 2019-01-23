2a006a1dbc7c6856af06eca3a64f1a53ba5f0c2a
Jae-eun <je4297@naver.com>
2019-01-23 17:30:29 +0900

    applyShadow, setRounded extension 추가

---
+++ FineDust/Extension/UIImageView+.swift
@@ -0,0 +1,16 @@
+//
+//  UIImageView+.swift
+//  FineDust
+//
+//  Created by 이재은 on 23/01/2019.
+//  Copyright © 2019 boostcamp3rd. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+  func setRounded() {
+    self.layer.cornerRadius = self.frame.height / 2
+    self.layer.masksToBounds = true
+  }
+}
