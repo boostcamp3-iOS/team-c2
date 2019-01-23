e1e6a9ffaf116376f4c96dfba133ac94f300cc6c
Jae-eun <je4297@naver.com>
2019-01-23 17:34:53 +0900

    applyGradient extension 추가

--- FineDust/Extension/CALayer+.swift
+++ FineDust/Extension/CALayer+.swift
@@ -40,4 +40,19 @@ extension CALayer {
       shadowPath = UIBezierPath(rect: rect).cgPath
     }
   }
+  
+  func applyGradient(
+    colors: [Any],
+    locations: [NSNumber],
+    startPoint: CGPoint = .init(x: 0.5, y: 0),
+    endPoint: CGPoint = .init(x: 0.5, y: 1)
+  ) {
+    let gradient = CAGradientLayer()
+    gradient.frame = bounds
+    gradient.startPoint = startPoint
+    gradient.endPoint = endPoint
+    gradient.colors = colors
+    gradient.locations = locations
+    mask = gradient
+  }
 }
