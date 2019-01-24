d45c4ce04b6504b3f6f66ff8eb4d0e63d83ac219
Jae-eun <je4297@naver.com>
2019-01-24 10:55:42 +0900

    FeedbackListVC 수정

--- FineDust/Feedback/Controller/FeedbackListViewController.swift
+++ FineDust/Feedback/Controller/FeedbackListViewController.swift
@@ -9,60 +9,63 @@
 import UIKit
 
 final class FeedbackListViewController: UIViewController {
-
-  @IBOutlet weak var feedbackCollectionView: UICollectionView!
-  @IBOutlet weak var feedbackListTabelView: UITableView!
   
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    navigationItem.title = "먼지 정보"
-
-    feedbackCollectionView.reloadData()
-    feedbackListTabelView.reloadData()
-  }
-
+  // Mark: IBOutlet
+  @IBOutlet private weak var feedbackCollectionView: UICollectionView!
+  @IBOutlet private weak var feedbackListTabelView: UITableView!
+  
+  // Mark: Properties
   private let reuseIdentifiers = ["feedbackCell", "feedbackListCell"]
   private var count = 10
   private let cornerRadius: CGFloat = 7
-
+  
+  // Mark: - LifeCycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    navigationItem.title = "먼지 정보"
+    
+    feedbackCollectionView.reloadData()
+    feedbackListTabelView.reloadData()
+  }
 }
-    // MARK: - UICollectionViewDataSource
+// MARK: - UICollectionViewDataSource
 
 extension FeedbackListViewController: UICollectionViewDataSource {
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-
-    func collectionView(
-      _ collectionView: UICollectionView,
-      numberOfItemsInSection section: Int
-      ) -> Int {
-        return 3
-    }
-
-    func collectionView(
-      _ collectionView: UICollectionView,
-      cellForItemAt indexPath: IndexPath
-      ) -> UICollectionViewCell {
-
-      guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifiers[0], for: indexPath)
-        as? FeedbackCollectionViewCell else {
-          return UICollectionViewCell()
-      }
-      cell.feedbackImageView.layer.cornerRadius = cornerRadius
-      cell.feedbackImageView.layer.masksToBounds = true
-      cell.feedbackImageView.image = UIImage(named: "info1")
-      
-      cell.feedbackTitleLabel.text = "미세먼지 정화 식물"
-      cell.feedbackTitleLabel.layer.cornerRadius = cornerRadius
-      cell.feedbackTitleLabel.layer.masksToBounds = true
-  
-        return cell
-    }
+  func numberOfSections(in collectionView: UICollectionView) -> Int {
+    return 1
   }
-    // MARK: - UICollectionViewDelegate
+  
+  func collectionView(
+    _ collectionView: UICollectionView,
+    numberOfItemsInSection section: Int
+  ) -> Int {
+    return 3
+  }
+  
+  func collectionView(
+    _ collectionView: UICollectionView,
+    cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+    
+    guard let cell = collectionView.dequeueReusableCell(
+      withReuseIdentifier: reuseIdentifiers[0],
+      for: indexPath
+    ) as? FeedbackCollectionViewCell
+    else { return UICollectionViewCell() }
+    
+    cell.feedbackImageView.layer.cornerRadius = cornerRadius
+    cell.feedbackImageView.layer.masksToBounds = true
+    cell.feedbackImageView.image = UIImage(named: "info1")
+    
+    cell.feedbackTitleLabel.text = "미세먼지 정화 식물"
+    cell.feedbackTitleLabel.layer.cornerRadius = cornerRadius
+    cell.feedbackTitleLabel.layer.masksToBounds = true
+    
+    return cell
+  }
+}
 
-    // MARK: - UITabelViewDataSource
+// MARK: - UITabelViewDataSource
 
 extension FeedbackListViewController: UITableViewDataSource {
   func numberOfSections(in tableView: UITableView) -> Int {
@@ -83,11 +86,17 @@ extension FeedbackListViewController: UITableViewDataSource {
     cell.feedbackImageView.image = UIImage(named: "info1")
     cell.feedbackTitleLabel.text = "미세먼지 정화 식물"
     cell.feedbackSourceLabel.text = "KTV 국민 방송"
-
+    
     cell.feedbackImageView.setRounded()
     
     cell.feedbackListShadowView.layer.applySketchShadow(
-      color: UIColor.gray, alpha: 0.2, x: 0, y: 0, blur: 5, spread: 3)
+      color: UIColor.gray,
+      alpha: 0.2,
+      x: 0,
+      y: 0,
+      blur: 5,
+      spread: 3
+    )
     cell.feedbackListShadowView.layer.cornerRadius = 5
     
     return cell
