8680f9a8eb25900805305dd7eac51b8c01902d11
presto <yoohan95@gmail.com>
2019-01-25 16:14:49 +0900

    리뷰 반영

--- FineDust/Core Data/CoreDataHelper.swift
+++ FineDust/Core Data/CoreDataHelper.swift
@@ -12,15 +12,19 @@ import UIKit
 
 
 /// Core Data Helper
-class CoreDataHelper {
+final class CoreDataHelper {
+  
+  // MARK: Singleton Object
   
   static let shared = CoreDataHelper()
   
+  /// AppDelegate에 있는 viewContext 가져옴
   private lazy var context: NSManagedObjectContext = {
     guard let delegate = UIApplication.shared.delegate as? AppDelegate else { fatalError() }
     return delegate.persistentContainer.viewContext
   }()
   
+  /// 특정 타입의 데이터 fetch하는 메소드
   func fetch<T: NSManagedObject>(forType type: T.Type, _ completion: (T?, Error?) -> Void) {
     let request = NSFetchRequest<NSFetchRequestResult>(entityName: T.classNameToString)
     do {
@@ -31,6 +35,7 @@ class CoreDataHelper {
     }
   }
   
+  /// 특정 타입에 전달된 데이터를 저장하는 메소드
   func save<T: NSManagedObject>(
     _ dictionary: [String: Any],
     forType type: T.Type,
