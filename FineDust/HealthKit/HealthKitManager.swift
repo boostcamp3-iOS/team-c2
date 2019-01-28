c131c979a9b6205e35c8f3b2dce4796b86caeb34
Jae-eun <je4297@naver.com>
2019-01-28 18:01:14 +0900

    함수명 변경 return -> fetch, 주석 추가

--- FineDust/HealthKit/HealthKitManager.swift
+++ FineDust/HealthKit/HealthKitManager.swift
@@ -8,25 +8,24 @@
 
 import Foundation
 import HealthKit
-
+/// HealthKitManagerType 프로토콜 선언
 protocol HealthKitManagerType {
   func findHealthKitValue(
     startDate: Date,
     endDate: Date,
     quantityFor: HKUnit,
     quantityTypeIdentifier: HKQuantityTypeIdentifier,
-    completion: @escaping (_ quantityValue: Double) -> Void)
-  func returnDistanceValue(_ completion: @escaping (Double) -> Void)
-  func returnStepCountValue(_ completion: @escaping (Double) -> Void)
+    completion: @escaping (Double) -> Void)
+  func fetchDistanceValue(_ completion: @escaping (Double) -> Void)
+  func fetchStepCountValue(_ completion: @escaping (Double) -> Void)
 }
-
+/// HealthKitManager 기능 구현 부분
 struct HealthKitManager: HealthKitManagerType {
   // HealthKit Data에 접근하는 지점.
-  fileprivate let healthStore = HKHealthStore()
-  fileprivate let calendar = Calendar(identifier: Calendar.Identifier.gregorian)
+  private let healthStore = HKHealthStore()
+  private let calendar = Calendar(identifier: Calendar.Identifier.gregorian)
   let startDate = Calendar.current.date(bySettingHour: 0, minute: 0, second: 0, of: Date())
   let endDate = Date()
-  
   /// HealthKit 값 가져오는 함수.
   func findHealthKitValue(
     startDate: Date,
@@ -41,18 +40,17 @@ struct HealthKitManager: HealthKitManagerType {
         withStart: startDate,
         end: endDate,
         options: .strictStartDate)
-      
       // 가져올 날짜 단위 변수.
       var interval: DateComponents = DateComponents()
       interval.day = 1
-      
       // 정한 시간에 대한 통계 쿼리를 수행하고 결과를 반환함.
-      let query = HKStatisticsCollectionQuery(quantityType: quantityType,
-                                              quantitySamplePredicate: predicate,
-                                              options: [.cumulativeSum],
-                                              anchorDate: startDate,
-                                              intervalComponents: interval)
-      
+      let query = HKStatisticsCollectionQuery(
+        quantityType: quantityType,
+        quantitySamplePredicate: predicate,
+        options: [.cumulativeSum],
+        anchorDate: startDate,
+        intervalComponents: interval
+      )
       query.initialResultsHandler = { query, results, error in
         if error != nil {
           print("findHealthKitValue error: \(String(describing: error?.localizedDescription))")
@@ -78,11 +76,9 @@ struct HealthKitManager: HealthKitManagerType {
       healthStore.execute(query)
     }
   }
-  
-  /// 걸은 거리 반환하기.
-  func returnDistanceValue(_ completion: @escaping (Double) -> Void) {
+  /// 걸은 거리 가져오기.
+  func fetchDistanceValue(_ completion: @escaping (Double) -> Void) {
     guard let startDate = startDate else { return }
-    
     findHealthKitValue(
       startDate: startDate,
       endDate: endDate,
@@ -91,11 +87,9 @@ struct HealthKitManager: HealthKitManagerType {
       completion: completion
     )
   }
-  
-  /// 걸음 수 반환하기.
-  func returnStepCountValue(_ completion: @escaping (Double) -> Void) {
+  /// 걸음 수 가져오기.
+  func fetchStepCountValue(_ completion: @escaping (Double) -> Void) {
     guard let startDate = startDate else { return }
-    
     findHealthKitValue(
       startDate: startDate,
       endDate: endDate,
