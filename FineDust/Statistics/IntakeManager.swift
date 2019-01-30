1c7aba0176b56f0c3390251e4699b1b18165613c
presto <yoohan95@gmail.com>
2019-01-30 10:24:21 +0900

    리뷰 반영

--- FineDust/Statistics/IntakeManager.swift
+++ FineDust/Statistics/IntakeManager.swift
@@ -8,20 +8,10 @@
 
 import Foundation
 
-// 1. IntakeGeneratorType 만들어서 함수 정의
-protocol IntakeManagerType {
-  
-  /// 평균 보폭
-  var averageStride: Double { get }
-  
-  /// 최근 7일의 흡입량 계산하여 컴플리션 핸들러에 넘겨줌
-  func calculateIntakesInWeek(since date: Date, completion: @escaping (Double) -> Void)
-}
-
 // 2. IntakeGenerator가 1의 프로토콜을 준수하고 빌드가 가능하게만 구현해둠
 class IntakeManager: IntakeManagerType {
   
-  var healthKitManager: HealthKitManagerType
+  var healthKitManager: HealthKitServiceManagerType
   
   var api: APIFineDustType
   
@@ -30,7 +20,7 @@ class IntakeManager: IntakeManagerType {
     return 60
   }
   
-  init(healthKitManager: HealthKitManagerType = HealthKitManager(),
+  init(healthKitManager: HealthKitServiceManagerType = HealthKitManager.shared,
        apiService: APIFineDustType = API.shared) {
     self.healthKitManager = healthKitManager
     self.api = apiService
@@ -42,7 +32,9 @@ class IntakeManager: IntakeManagerType {
     // 둘 사이에 어떠한 알고리즘 적용하여 값 넘겨줌
     healthKitManager.fetchDistanceValue { [weak self] distance in
       self?.healthKitManager.fetchStepCountValue { steps in
-        self?.api.fetchFineDustConcentration(term: .daily, pageNumber: 1, numberOfRows: 24) { response, error in
+        self?.api.fetchFineDustConcentration(term: .daily,
+                                             pageNumber: 1,
+                                             numberOfRows: 24) { response, error in
           let distance = distance
           let steps = steps
           let fineDustValue = Double(response?.items.first?.fineDustValue ?? 0)
