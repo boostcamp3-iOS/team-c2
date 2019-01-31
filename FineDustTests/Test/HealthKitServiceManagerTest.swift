7d75fd56e6d3ae320f37d329f0e1e2c0ab734683
Jae-eun <je4297@naver.com>
2019-01-31 13:50:52 +0900

    HealthKitServiceManager TestCase 만듦. find 함수 변경 끝난 후에 다시 수정하겠습니다.

--- FineDustTests/Test/HealthKitServiceManagerTest.swift
+++ FineDustTests/Test/HealthKitServiceManagerTest.swift
@@ -11,9 +11,7 @@ import XCTest
 import HealthKit
 
 class HealthKitServiceManagerTest: XCTestCase {
-  
-//  var healthKitServiceManager = HealthKitServiceManager()
-  
+
   var mockHealthKitManager = MockHealthKitManager()
   
   var expectedResult = 0.0
@@ -26,6 +24,7 @@ class HealthKitServiceManagerTest: XCTestCase {
     
   }
   
+  /// 걸음수 결과 반환을 테스트함.
   func testCheckStepCount() {
     mockHealthKitManager.findHealthKitValue(startDate: Date(),
                                             endDate: Date(),
@@ -35,8 +34,11 @@ class HealthKitServiceManagerTest: XCTestCase {
       self.expectedResult = value
     }
     XCTAssertEqual(2314.0, expectedResult, "stepCount should be 2314.0")
+    // error 보내는 부분 추가 되면 주석 풀겠습니다.
+//    XCTAssertTrue(error.localizedDescription == HKError(.noError).localizedDescription)
   }
   
+  /// 걸은 거리 결과 반환을 테스트함.
   func testChecktDistance() {
     mockHealthKitManager.findHealthKitValue(startDate: Date(),
                                             endDate: Date(),
@@ -46,8 +48,11 @@ class HealthKitServiceManagerTest: XCTestCase {
       self.expectedResult = value
     }
     XCTAssertEqual(1409.53, expectedResult, "distance should be 1409.53")
+    // error 보내는 부분 추가 되면 주석 풀겠습니다.
+    //    XCTAssertTrue(error.localizedDescription == HKError(.noError).localizedDescription)
   }
   
+  /// 나머지 identifier를 테스트함.
   func testCheckQuantityType() {
     mockHealthKitManager.findHealthKitValue(startDate: Date(),
                                             endDate: Date(),
@@ -58,4 +63,28 @@ class HealthKitServiceManagerTest: XCTestCase {
     }
     XCTAssertEqual(0, expectedResult, "stepCount should be 0")
   }
+  
+  /// HealthKit 데이터를 찾는 함수에 유효하지 않은 인자가 들어갔을 때 오류를 테스트함. 3
+  func testfindHealthKitValueInvalidArgument() {
+    mockHealthKitManager.findHealthKitValue(startDate: Date(),
+                                            endDate: Date(),
+                                            quantityFor: HKUnit.hour(),
+                                            quantityTypeIdentifier: .stepCount) {
+                                              (value) -> Void in
+                                              self.expectedResult = value
+    }
+    // error 보내는 부분 추가 되면 주석 풀겠습니다.
+//    XCTAssertTrue(error.localizedDescription == HKError(.errorInvalidArgument).localizedDescription)
+  }
+  
+  /// HealthKit 권한 여부를 테스트함. 4 / 5 / 6
+  func testHealthKitAuthrization() {
+    
+  }
+  
+  /// startDate 보다 endDate가 이전 날짜.
+  func testFindHealthKitValueInWrongDate() {
+    
+  }
+  
 }
