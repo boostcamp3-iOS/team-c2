05dcbf51d60f05ffe41ce0ba52b254c59cca9241
Jae-eun <je4297@naver.com>
2019-02-07 01:35:17 +0900

    FeedbackListTableViewCell 수정

--- FineDust/Feedback/View/FeedbackListTableViewCell.swift
+++ FineDust/Feedback/View/FeedbackListTableViewCell.swift
@@ -14,11 +14,9 @@ final class FeedbackListTableViewCell: UITableViewCell {
   @IBOutlet private weak var feedbackTitleLabel: UILabel!
   @IBOutlet private weak var feedbackSourceLabel: UILabel!
   @IBOutlet private weak var feedbackListShadowView: UIView!
-  @IBOutlet private weak var feedbackListTitleLabel: UILabel!
   @IBOutlet private weak var bookmarkButton: UIButton!
   
   func setProperties(at index: Int) {
-    feedbackListTitleLabel.isHidden = index != 0 ? true : false
     feedbackImageView.image = UIImage(named: "info1")
     feedbackTitleLabel.text = "미세먼지 정화 식물"
     feedbackSourceLabel.text = "KTV 국민 방송"
