07a63fa772b3ff7a6beb408f389d41caeb174bb3
Jae-eun <je4297@naver.com>
2019-02-09 16:57:47 +0900

    DustFeedback.json 수정 / DustFeedback struct 정의 / notification 추가

---
+++ FineDust/Feedback/DustFeedback.swift
@@ -0,0 +1,19 @@
+//
+//  DustFeedback.swift
+//  FineDust
+//
+//  Created by 이재은 on 07/02/2019.
+//  Copyright © 2019 boostcamp3rd. All rights reserved.
+//
+
+import Foundation
+
+/// 'DustFeedback' json 
+struct DustFeedback: Codable {
+  
+  let title: String
+  let author: String
+  let date: String
+  let contents: String
+  
+}
