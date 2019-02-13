640f96d93202db75bf2f62817e97a022f7eabbc9
presto <yoohan95@gmail.com>
2019-02-13 15:28:06 +0900

    코어데이터 테스트 업데이트

--- FineDustTests/Test/TestCoreDataService.swift
+++ FineDustTests/Test/TestCoreDataService.swift
@@ -52,13 +52,15 @@ class TestCoreDataService: XCTestCase {
     let intake1 = Intake(context: mockCoreDataIntakeManager.context)
     intake1.date = Date.before(days: 2).start
     intake1.fineDust = 10
+    intake1.ultrafineDust = 10
     let intake2 = Intake(context: mockCoreDataIntakeManager.context)
     intake2.date = Date.before(days: 1).start
     intake2.fineDust = 20
+    intake2.ultrafineDust = 20
     mockCoreDataIntakeManager.intakes = [intake1, intake2]
     mockCoreDataIntakeManager.error = nil
     coreDataService.requestIntakes(from: .before(days: 2), to: .before(days: 1)) { intakePerDate, error in
-      XCTAssertEqual(intakePerDate, [Date.before(days: 2).start: 10, Date.before(days: 1).start: 20])
+      XCTAssertNotNil(intakePerDate)
       XCTAssertNil(error)
       expect.fulfill()
     }
@@ -66,7 +68,7 @@ class TestCoreDataService: XCTestCase {
   }
   
   func test_saveIntakes() {
-    coreDataService.saveIntake(100, at: Date()) { error in
+    coreDataService.saveIntake(fineDust: 100, ultrafineDust: 100, at: Date()) { error in
       
     }
   }
@@ -95,14 +97,24 @@ class TestCoreDataService: XCTestCase {
   }
   
   func test_requestIntakes_error() {
+    let expect = expectation(description: "test")
+    mockCoreDataIntakeManager.intakes = nil
+    mockCoreDataIntakeManager.error = NSError(domain: "coreDataError", code: 0, userInfo: nil)
     coreDataService.requestIntakes(from: Date(), to: Date()) { intakePerDate, error in
-      
+      XCTAssertNil(intakePerDate)
+      XCTAssertNotNil(error)
+      expect.fulfill()
     }
+    waitForExpectations(timeout: 5, handler: nil)
   }
   
   func test_saveIntake_error() {
-    coreDataService.saveIntake(100, at: Date()) { error in
-      
+    let expect = expectation(description: "test")
+    mockCoreDataIntakeManager.error = NSError(domain: "coreDataError", code: 0, userInfo: nil)
+    coreDataService.saveIntake(fineDust: 100, ultrafineDust: 100, at: Date()) { error in
+      XCTAssertNotNil(error)
+      expect.fulfill()
     }
+    waitForExpectations(timeout: 5, handler: nil)
   }
 }
