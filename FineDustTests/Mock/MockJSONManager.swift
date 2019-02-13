a42ecb7404df2ca04cede4264e54d0eec9712ae7
Jae-eun <je4297@naver.com>
2019-02-13 13:21:46 +0900

    MockJSONManager 만듦

---
+++ FineDustTests/Mock/MockJSONManager.swift
@@ -0,0 +1,22 @@
+//
+//  MockJSONManager.swift
+//  FineDustTests
+//
+//  Created by 이재은 on 12/02/2019.
+//  Copyright © 2019 boostcamp3rd. All rights reserved.
+//
+
+import Foundation
+
+@testable import FineDust
+import Foundation
+
+final class MockJSONManager: JSONManagerType {
+
+  let emptyJSONData = {[]}
+  let mockJSONData = {[
+  {"title": "미", "imageName": "T", "source": "K", "date": "2018-12-15", "contents": " 틸"},
+  {"title": "미", "imageName": "T", "source": "K", "date": "2018-12-15", "contents": " 틸"},
+  {"title": "미", "imageName": "T", "source": "K", "date": "2018-12-15", "contents": " 틸"},
+  {"title": "미", "imageName": "T", "source": "K", "date": "2018-12-15", "contents": " 틸"}
+  ]}
