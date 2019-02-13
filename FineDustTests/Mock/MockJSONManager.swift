d0b25f0d6b42c9849d71b82e5dc75dabc9fc65a3
Jae-eun <je4297@naver.com>
2019-02-13 14:31:24 +0900

    MockJSONManager Dummy 삭제

--- FineDustTests/Mock/MockJSONManager.swift
+++ FineDustTests/Mock/MockJSONManager.swift
@@ -2,7 +2,7 @@
 //  MockJSONManager.swift
 //  FineDustTests
 //
-//  Created by 이재은 on 12/02/2019.
+//  Created by Presto on 12/02/2019.
 //  Copyright © 2019 boostcamp3rd. All rights reserved.
 //
 
@@ -14,14 +14,6 @@ import Foundation
 final class MockJSONManager: JSONManagerType {
 
   var dustFeedbacks: [DustFeedback] = []
-  
-  let emptyJSONData = {[]}
-  let mockJSONData = {[
-  {"title": "미", "imageName": "T", "source": "K", "date": "2018-12-15", "contents": " 틸"},
-  {"title": "미", "imageName": "T", "source": "K", "date": "2018-12-15", "contents": " 틸"},
-  {"title": "미", "imageName": "T", "source": "K", "date": "2018-12-15", "contents": " 틸"},
-  {"title": "미", "imageName": "T", "source": "K", "date": "2018-12-15", "contents": " 틸"}
-  ]}
   
   func fetchDustFeedbacks() -> [DustFeedback] {
     return dustFeedbacks
