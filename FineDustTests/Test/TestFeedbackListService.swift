623278e9ff81462695dee16b1e06f0efafd4717f
Jae-eun <je4297@naver.com>
2019-02-15 03:01:43 +0900

    DummyJSONManager 더미데이터 추가 / ServiceTest 미완입니다.

--- FineDustTests/Test/TestFeedbackListService.swift
+++ FineDustTests/Test/TestFeedbackListService.swift
@@ -12,34 +12,53 @@ import Foundation
 
 class FeedbackListServiceTest: XCTestCase {
   
-  var feedbackListService: FeedbackListService?
+  var feedbackListService: FeedbackListService!
   let mockJSONManager = MockJSONManager()
   
   override func setUp() {
+    mockJSONManager.dustFeedbacks = DummyJSONManager.JSONData
     feedbackListService = FeedbackListService(jsonManager: mockJSONManager)
     
   }
   
   /// 오늘 피드백 데이터 개수 받아오는 함수 테스트
   func testFeedbackCount() {
-//    mockJSONManager.dustFeedbacks = mockJSONManager.fetchDustFeedbacks() DummyJSONManager.JSONData
-    let result = feedbackListService?.fetchFeedbackCount()
-    XCTAssertEqual(result, mockJSONManager.dustFeedbacks.count)
-
+//    let result = feedbackListService.fetchFeedbackCount()
+//    XCTAssertEqual(result, mockJSONManager.dustFeedbacks.count)
   }
   
   /// 해당 인덱스의 피드백 정보를 반환하는 함수 테스트
   func testFetchFeedbackData() {
-
+//    let index = 0
+//    let result = feedbackListService.fetchFeedbackData(at: index)
+//      XCTAssertEqual(result, mockJSONManager.dustFeedbacks[index])
+    
   }
   
   /// 피드백 정보를 최신순으로 반환하는 함수 테스트
   func testFetchFeedbackResentDate(){
-
+    
   }
   
   /// 피드백 정보를 제목순으로 반환하는 함수 테스트
   func testFetchFeedbackTitle() {
-
+    
   }
+  
+  /// 즐겨찾기한 글의 제목을 저장하여 배열 처리하는 함수 테스트
+  func testSetBookmarkInfoTitleArray() {
+    
+    
+  }
+  
+  /// 즐겨찾기한 글의 제목으로 인덱스를 반환하는 함수 테스트
+  func testGetBookmarkInfoIndex() {
+    
+  }
+  
+  /// 피드백 정보를 즐겨찾기순으로 반환하는 함수 테스트
+  func testFetchFeedbackBookmark() {
+    
+  }
+  
 }
