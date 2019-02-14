0acea9cffa367f671a9cd660fd4c81630a7848d4
zunzunzun <zun8ang@gmail.com>
2019-02-14 11:12:53 +0900

    Notification 이름 추가

--- FineDust/Extension/Notification.Name+.swift
+++ FineDust/Extension/Notification.Name+.swift
@@ -19,5 +19,16 @@ extension Notification.Name {
   
   /// 위치정보 권한 성공 이외 노티피케이션
   static let locationPermissionDenied = Notification.Name("locationPermissionDenied")
-
+  
+  /// HealthKit 권한이 결정되지 않음 노티피케이션
+  static let healthKitAuthorizationNotDetermined
+    = Notification.Name("healthKitAuthorizationNotDetermined")
+  
+  /// HealthKit 권한 거부 노티피케이션
+  static let healthKitAuthorizationSharingDenied
+    = Notification.Name("healthKitAuthorizationSharingDenied")
+  
+  /// HealthKit 권한 허용 노티피케이션
+  static let healthKitAuthorizationSharingAuthorized
+    = Notification.Name("healthKitAuthorizationSharingAuthorized")
 }
