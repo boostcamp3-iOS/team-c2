55977824d3bba5c2a6b0a496639ebcf937838d96
Jae-eun <je4297@naver.com>
2019-02-14 16:24:22 +0900

    즐겨찾기순 정렬 기능 추가

--- FineDust/Feedback/Controller/FeedbackListService.swift
+++ FineDust/Feedback/Controller/FeedbackListService.swift
@@ -11,13 +11,19 @@ import Foundation
 /// FeedbackListService를 구현하는 클래스
 final class FeedbackListService: FeedbackServiceType {
   
-  private var dustFeedbacks: [DustFeedback] = []
+  // MARK: - Properties
+  
   let jsonManager: JSONManagerType
+  private var dustFeedbacks: [DustFeedback] = []
+  private var sortedArray: [DustFeedback] = []
+  private var bookmarkInfoTitleArray: [String] = []
   
   init(jsonManager: JSONManagerType) {
     self.jsonManager = jsonManager
     dustFeedbacks =  jsonManager.fetchDustFeedbacks()
   }
+  
+  // MARK: - Functions
   
   /// 피드백 정보의 개수를 반환함.
   func fetchFeedbackCount() -> Int {
@@ -33,15 +39,59 @@ final class FeedbackListService: FeedbackServiceType {
   
   /// 피드백 정보를 최신순으로 반환함
   func fetchFeedbackRecentDate() -> [DustFeedback] {
-    let sortedArray = dustFeedbacks.sorted(by: { $0.date > $1.date })
-    print(sortedArray)
+    sortedArray = dustFeedbacks.sorted(by: { $0.date > $1.date })
     return sortedArray
   }
   
   /// 피드백 정보를 제목순으로 반환함
   func fetchFeedbackTitle() -> [DustFeedback] {
-    let sortedArray = dustFeedbacks.sorted(by: { $0.title < $1.title })
+    sortedArray = dustFeedbacks.sorted(by: { $0.title < $1.title })
+    return sortedArray
+  }
+  
+  /// 즐겨찾기한 글의 제목을 저장하여 배열 처리함.
+  func setBookmarkInfoTitleArray(title: String) {
+    UserDefaults.standard.set(title, forKey: "bookmarkInfoTitle")
+    if let bookmarkInfoTitle = UserDefaults.standard.string(forKey: "bookmarkInfoTitle") {
+      bookmarkInfoTitleArray.append(bookmarkInfoTitle)
+    }
+  }
+  
+  /// 즐겨찾기한 글의 제목으로 인덱스를 반환함
+  func getBookmarkInfoIndex() -> [Int] {
+    var bookmarkIndex: [Int] = []
+    print(bookmarkIndex)
+    for index in 0..<bookmarkInfoTitleArray.count {
+      print("f\(bookmarkInfoTitleArray.count)")
+      for index2 in 0..<dustFeedbacks.count {
+        print("df\(dustFeedbacks.count)")
+        if dustFeedbacks[index2].title == bookmarkInfoTitleArray[index] {
+          print(dustFeedbacks.index(after: index2-1))
+          bookmarkIndex.append(dustFeedbacks.index(after: index2-1))
+          break
+        }
+      }
+    }
+    return bookmarkIndex
+  }
+  
+  /// 피드백 정보를 즐겨찾기순으로 반환함
+  func fetchFeedbackBookmark() -> [DustFeedback] {
+    var feedbackIndex = Array<Int>(0..<dustFeedbacks.count)
+    let bookmarkIndexArray = getBookmarkInfoIndex()
     
+    for index2 in (0..<bookmarkIndexArray.count).reversed() {
+      sortedArray.append(dustFeedbacks[bookmarkIndexArray[index2]])
+      for index in 0..<dustFeedbacks.count {
+        if bookmarkIndexArray[index2] == feedbackIndex[index] {
+          feedbackIndex.remove(at: index)
+          break
+        }
+      }
+    }
+    for index2 in 0..<feedbackIndex.count {
+      sortedArray.append(dustFeedbacks[feedbackIndex[index2]])
+    }
     return sortedArray
   }
   
